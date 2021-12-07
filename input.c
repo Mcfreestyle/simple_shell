c3a59df5e35d6b9f5f6df1275becc7cf8a461f97
Mcfreestyle <mcfreestyle.go1@gmail.com>
2021-12-07 15:51:15 -0800

    feat: gets the input and store it into array

---
+++ input.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * get_input - gets the input and store it into array
+ * @argv : array that will has the input
+ */
+void get_input(char *argv[])
+{
+	char *buff = NULL;
+	char *token;
+	size_t sizbuff = 100;
+	int i, input;
+
+	input = getline(&buff, &sizbuff, stdin);
+	if (input == EOF)
+	{
+		free(buff);
+		exit(1);
+	}
+	token = strtok(buff, " ");
+	for (i = 0; token != NULL; i++)
+	{
+		argv[i] = _strdup(token);
+		token = strtok(NULL, " ");
+	}
+	if (_strcmp(argv[i - 1], "\n") != 0)
+	{
+		strtok(argv[i - 1], "\n");
+		argv[i] = NULL;
+	}
+	else
+	{
+		free(argv[i - 1]);
+		argv[i - 1] = NULL;
+	}
+	free(buff);
+}
